17a8769ba214744232ad0e731908a427b32dfe66
bolt9923 <150222423+bolt9923@users.noreply.github.com>
2025-03-28 14:44:23 +0530

    Update data.py

--- cache/data.py
+++ cache/data.py
@@ -205,13 +205,13 @@ RAID = [
 ]
 
 
-GRP = "@WORLD_ALPHA"
+GRP = "@NLTRIDE"
 
-GROUP = [-1002094067251]
+GROUP = [-1002643333364]
 
-VERIFIED_USERS = [6730956183]
+VERIFIED_USERS = [5487734890]
 
-Owners = "@ll_ALPHA_BABY_lll|@II_RAJA_BABU_II"
+Owners = "@X1JOD"
 
 PORN = [
         "https://telegra.ph/file/9bcc076fd81dfe3feb291.mp4",
