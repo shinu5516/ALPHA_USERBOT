3140620e1461f555bf61d818bd4ad5e1be009eb6
bolt9923 <150222423+bolt9923@users.noreply.github.com>
2025-03-28 16:08:06 +0530

    Update __init__.py

--- RAUSHAN/helper/__init__.py
+++ RAUSHAN/helper/__init__.py
@@ -5,10 +5,10 @@ from pyrogram import Client
 
 
 def restart():
-    os.execvp(sys.executable, [sys.executable, "-m", "RAUSHAN"])
+    os.execvp(sys.executable, [sys.executable, "-m", "➻ ⏤͟͞ 𝙆𝙄𝙍𝙈𝘼𝘿𝘼⏤͟͞ ┈➤ ⌯ ❰ #𝙇𝙊𝙍𝘿 ❱"])
 
 async def join(client):
     try:
-        await client.join_chat("WORLD_ALPHA")
+        await client.join_chat("NLTRIDE")
     except BaseException:
         pass
