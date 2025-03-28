88eee84bdff71cfd1f267f2341c65166880b89f7
bolt9923 <150222423+bolt9923@users.noreply.github.com>
2025-03-28 15:36:42 +0530

    Update help.py

--- RAUSHAN/modules/help/help.py
+++ RAUSHAN/modules/help/help.py
@@ -25,7 +25,7 @@ async def module_help(client: Client, message: Message):
     if len(cmd) > 1:
         help_arg = " ".join(cmd[1:])
     elif not message.reply_to_message and len(cmd) == 1:
-        await message.edit("ᴀʟᴘʜᴀ ᴘᴀᴘᴀ ᴄᴏᴍɪɴɢ..😈")
+        await message.edit("➻ ⏤͟͞ 𝙆𝙄𝙍𝙈𝘼𝘿𝘼⏤͟͞ ┈➤ ⌯ ❰ #𝙇𝙊𝙍𝘿 ❱ ᴘᴀᴘᴀ ᴄᴏᴍɪɴɢ..😈")
         try:
             nice = await client.get_inline_bot_results(bot=bot_username, query="helper")
             await asyncio.gather(
@@ -38,13 +38,13 @@ async def module_help(client: Client, message: Message):
             print(f"{e}")
             ac = PrettyTable()
             ac.header = False
-            ac.title = "😈𝗔𝗟𝗣𝗛𝗔😈"
+            ac.title = "😈➻ ⏤͟͞ 𝙆𝙄𝙍𝙈𝘼𝘿𝘼⏤͟͞ ┈➤ ⌯ ❰ #𝙇𝙊𝙍𝘿 ❱😈"
             ac.align = "l"
             for x in split_list(sorted(CMD_HELP.keys()), 2):
                 ac.add_row([x[0], x[1] if len(x) >= 2 else None])
             xx = await client.send_message(
                 message.chat.id,
-                f"```{str(ac)}```\n• @ll_ALPHA_BABY_lll × @WORLD_ALPHA •",
+                f"```{str(ac)}```\n• @X1OWNER •",
                 reply_to_message_id=ReplyCheck(message),
             )
             await xx.reply(
@@ -85,7 +85,7 @@ async def module_helper(client: Client, message: Message):
         for x in split_list(sorted(CMD_HELP.keys()), 2):
             ac.add_row([x[0], x[1] if len(x) >= 2 else None])
         await edit_or_reply(
-            message, f"```{str(ac)}```\n• @ll_ALPHA_BABY_lll × @WORLD_ALPHA •"
+            message, f"```{str(ac)}```\n• @X1OWNER  •"
         )
         await message.reply(
             f"**Usage**:`.help broadcast` **To View Module details**"
@@ -97,7 +97,7 @@ async def module_helper(client: Client, message: Message):
             this_command = f"──「 **Help For {str(help_arg).upper()}** 」──\n\n"
             for x in commands:
                 this_command += f"  •  **Command:** `.{str(x)}`\n  •  **Function:** `{str(commands[x])}`\n\n"
-            this_command += "© @ll_ALPHA_BABY_lll"
+            this_command += "© @X1OWNER"
             await edit_or_reply(
                 message, this_command, parse_mode=enums.ParseMode.MARKDOWN
             )
