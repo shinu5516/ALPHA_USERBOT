360a36bf889ae3832c7b54aa7ef9e08fd021c215
shinu5516 <tgmusic5516@gmail.com>
2025-06-07 15:53:15 +0530

    Update config.py

--- config.py
+++ config.py
@@ -18,7 +18,7 @@ ALIVE_TEXT = getenv("ALIVE_TEXT")
 PM_LOGGER = getenv("PM_LOGGER")
 LOG_GROUP = getenv("LOG_GROUP")
 GIT_TOKEN = getenv("GIT_TOKEN") #personal access token
-REPO_URL = getenv("REPO_URL", "https://github.com/bolt9923/ALPHA_USERBOT")
+REPO_URL = getenv("REPO_URL", "https://github.com/shinu5516/ALPHA_USERBOT")
 BRANCH = getenv("BRANCH", "main") #don't change
  
 STRING_SESSION1 = getenv("STRING_SESSION1", "")
